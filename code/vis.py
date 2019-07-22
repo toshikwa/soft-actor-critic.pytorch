0f1ac968a266950c9cf9874a8eea499619929a52
Toshiki Watanabe <kuboy2482@gmail.com>
2019-07-23 04:26:45 +0900

    add visualizer

---
+++ code/vis.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_return_history(mean_history, std_history, save_path, interval=10):
+    # x axis
+    x = np.arange(interval, len(mean_history)*interval+1, interval)
+    tag = ['', ' (thousand)', ' (million)']
+    i_tag = 0
+
+    while interval >= 1000:
+        x = x / 1000
+        interval = interval / 1000
+        i_tag += 1
+
+    # plot
+    plt.figure()
+    plt.title(f"Return history")
+    plt.grid()
+    plt.fill_between(
+        x, mean_history - std_history, mean_history + std_history,
+        alpha=0.3)
+    plt.plot(x, mean_history, "-")
+    plt.xlabel(f"steps{tag[i_tag]}")
+    plt.ylabel(f"average return")
+    plt.savefig(save_path)
+    # plt.close()
